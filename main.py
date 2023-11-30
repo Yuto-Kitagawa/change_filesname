814f56b9adf5314a9fb4c8ffd4401045bc1517ee
Yuto-Kitagawa <80242045+Yuto-Kitagawa@users.noreply.github.com>
2023-11-30 10:16:13 +0900

    fix: 同じ名前のファイルがあるときにエラーが発生する問題を解消

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-# 同じディレクトリにfilesファイルを作成し、その中に名前を変更したいファイルを入れてください
+# 同じディレクトリにbeforeファイルを作成し、
+# その中に名前を変更したいファイルを入れてください
 
 import glob
 import os
+import shutil
 
 # ファイル拡張子
 # 変更前
@@ -16,12 +18,19 @@ change_before_file_name_regex = "*"
 # 変更後ファイル名(最後にインデックスが入ります)
 change_after_file_name = "Azure-構築-"
 
+## afterディレクトリ(変更後ディレクトリ)がなければ作成
+os.makedirs("./after", exist_ok=True)
 
-path = './files/' + change_before_file_name_regex + '.' + change_before_file_type
+for p in glob.glob('after/*', recursive=True):
+    if os.path.isfile(p):
+        os.remove(p)
+
+
+path = './before/' + change_before_file_name_regex + '.' + change_before_file_type
 i = 1
 
-flist = glob.glob(path)
+flist = sorted(glob.glob(path),key=os.path.getmtime,reverse=False)
 
 for file in flist:
-    os.rename(file, './files/' + change_after_file_name + str(i) + '.' + change_after_file_type)
+    os.rename(file, './after/' + change_after_file_name + str(i) + '.' + change_after_file_type)
     i += 1
