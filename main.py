b0771ffaf29a7fe911cd293a54bb297fe438ecc6
Yuto-Kitagawa <80242045+Yuto-Kitagawa@users.noreply.github.com>
2023-11-30 09:56:21 +0900

    fix: 拡張子・名前の入力を追加

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-#同じディレクトリにfilesファイルを作成し、その中に名前を変更したいファイルを入れてください
+# 同じディレクトリにfilesファイルを作成し、その中に名前を変更したいファイルを入れてください
 
 import glob
 import os
 
-# ファイル名
-path = './files/*.jpg'
+# ファイル拡張子
+# 変更前
+change_before_file_type = "png"
+
+# 変更後
+change_after_file_type = "png"
+
+# 変更前ファイル名(正規表現)
+change_before_file_name_regex = "*"
+
+# 変更後ファイル名(最後にインデックスが入ります)
+change_after_file_name = "Azure-構築-"
+
+
+path = './files/' + change_before_file_name_regex + '.' + change_before_file_type
 i = 1
 
 flist = glob.glob(path)
 
-# したい名前にする
-name = "carousel"
-
 for file in flist:
-    # ファイル名
-    os.rename(file, './files/' + name + str(i) + '.jpg')
+    os.rename(file, './files/' + change_after_file_name + str(i) + '.' + change_after_file_type)
     i += 1
